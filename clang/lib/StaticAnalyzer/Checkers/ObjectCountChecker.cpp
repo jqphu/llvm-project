58b19636ae97d1f7ab34f0b7c18c5870feb88868
Justin Phu <justinqphu@gmail.com>
2020-10-25 13:33:39 -0700

    [analyzer] Add the initial state for the analysis
    This diff introduces the basic state setup for our analysis.
    
    The basic analysis will only work in *input parameters and parameters
    returned by `_create` functions*. For the basic implementation we will
    not look into structures.
    
    This diff does not do any analysis yet.
    
    Test:
    `llvm-lit -sv clang/test/Analysis/object-count-checks.c` passes.
    
    * RefCount
    
    This is our state tracking for our static analyzer. It will increment
    with acquire calls and decrement with release calls. Each object
    will map directly to one of these RefCount states.
    
    It is fairly empty now but it will extend to include autorelease count
    and whether or not it has escaped the execution context.
    
    * NoAnalysisAnnotation
    
    Introduce an annotation to bootstrap the object count as well as be an
    escape hatch for cases we did not handle.
    
    * checkBeginFunction
    
    This is the main introduced function. It will run for every top level
    function analysis. It only operations on regular functions (no
    constructor, destructors, etc). It will initialize the object state
    for single pointer types (e.g. foo_t*, header_t*). This is because we
    currently have no way of identifying our objects.
    
    If the pointer type has the consumed attribute we initialize it as an
    owned object, if it doesn't we initialize it as unowned.

--- clang/lib/StaticAnalyzer/Checkers/ObjectCountChecker.cpp
+++ clang/lib/StaticAnalyzer/Checkers/ObjectCountChecker.cpp
@@ -17,27 +17,131 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "clang/AST/Attr.h"
 #include "clang/StaticAnalyzer/Checkers/BuiltinCheckerRegistration.h"
 #include "clang/StaticAnalyzer/Core/Checker.h"
+#include "clang/StaticAnalyzer/Core/PathSensitive/CallEvent.h"
+#include "clang/StaticAnalyzer/Core/PathSensitive/CheckerContext.h"
+#include "clang/StaticAnalyzer/Core/PathSensitive/ProgramStateTrait.h"
 
 using namespace clang;
 using namespace ento;
 
 namespace {
-class ObjectCountChecker : public Checker<check::BeginFunction> {
-public:
-  ObjectCountChecker();
 
+class RefCount {
+  ///
+  /// Reference count for this object.
+  ///
+  /// Ref count goes up everytime we call acquire, and goes down for
+  /// release/autorelease.
+  unsigned Cnt{0};
+
+  explicit RefCount(unsigned count) : Cnt(count){};
+
+public:
+  /// Helper to create a not owned RefCount.
+  ///
+  /// This has a reference count of 0.
+  LLVM_NODISCARD static RefCount makeUnowned() { return RefCount(0); }
+
+  /// Helper to create a owned RefCount.
+  ///
+  /// This object has a reference count of 1.
+  LLVM_NODISCARD static RefCount makeOwned() { return RefCount(1); }
+
+  /// Get the count for this object.
+  LLVM_NODISCARD unsigned getCount() const { return Cnt; }
+
+  bool operator==(const RefCount &Rhs) const { return Cnt == Rhs.getCount(); }
+
+  /// Used to create a hash for this node.
+  void Profile(llvm::FoldingSetNodeID &ID) const { ID.AddInteger(Cnt); }
+};
+
+class ObjectCountChecker : public Checker<check::BeginFunction> {
+  CallDescription ObjectAcquireFn{"object_acquire"};
+  CallDescription ObjectAutoreleaseFn{"object_autorelease"};
+  CallDescription ObjectCreateFn{"object_create"};
+  CallDescription ObjectReleaseFn{"object_release"};
+
+public:
   void checkBeginFunction(CheckerContext &C) const;
 };
 
 } // end anonymous namespace
 
-ObjectCountChecker::ObjectCountChecker() {}
+/// State of object symbol refs to their reference count.
+REGISTER_MAP_WITH_PROGRAMSTATE(ObjectRefCountMap, SymbolRef, RefCount)
 
-void ObjectCountChecker::checkBeginFunction(
-    CheckerContext &checkerContext) const {
-  // TODO: Fill in.
+/// Does this function decl have the no analysis annotation.
+///
+/// This detects if we have the
+/// __attribute__((annotate("object_no_anlaysis"))) on our function.  / This
+/// is used to bootstrap the object count checker as well as provide an
+/// escape hatch for false positives.
+LLVM_NODISCARD bool hasNoAnalysisAnnotation(const FunctionDecl *D) {
+  for (const auto *Ann : D->specific_attrs<AnnotateAttr>()) {
+    if (Ann->getAnnotation() == "object_no_analysis") {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+/// Called at the beginning of each function analysis.
+///
+/// This call will set up the State for all pointer input parameters. If the
+/// parameter is tagged with os_consumed we will initialize the State to have a
+/// reference count of 1. Otherwise, any other parameter will be initialize to a
+/// reference count of 0.
+///
+/// @note We initialize the State for all pointer input parameters regardless if
+/// they are an object or not. This is not optimal but will suffice.
+///
+/// Non pointer types are not initialized.
+void ObjectCountChecker::checkBeginFunction(CheckerContext &C) const {
+  // Only handle top-most framed functions. Do not analyzer beginning function.
+  if (!C.inTopFrame())
+    return;
+
+  const LocationContext *LCtx = C.getLocationContext();
+  const Decl *D = LCtx->getDecl();
+  const FunctionDecl *FD = dyn_cast_or_null<FunctionDecl>(D);
+
+  // Only operate on function declarations and ignore no_analysis function.
+  //
+  // Since we only care about C, we ignore constructors, destructors, blocks
+  // etc. This can be expanded by converting this to an AnyCall.
+  if (!FD || hasNoAnalysisAnnotation(FD))
+    return;
+
+  ProgramStateRef State = C.getState();
+  for (unsigned idx = 0, e = FD->param_size(); idx != e; ++idx) {
+    const ParmVarDecl *Param = FD->parameters()[idx];
+    QualType Ty = Param->getType();
+
+    // Only continue if it is a single pointer type.
+    if (!Ty->isPointerType() || Ty->getPointeeType()->isPointerType())
+      continue;
+
+    // Extract the region of memory of the parameter points to then extract a
+    // symbol to represent that. This is the key to our object.
+    SymbolRef Sym = State->getSVal(State->getRegion(Param, LCtx)).getAsSymbol();
+
+    if (Param->hasAttr<ObjectConsumedAttr>()) {
+      // Generate an object whose reference is owned by this function.
+      State = State->set<ObjectRefCountMap>(Sym, RefCount::makeOwned());
+    } else {
+      // Generate an object whose reference is not owned by this function.
+      State = State->set<ObjectRefCountMap>(Sym, RefCount::makeUnowned());
+    }
+  }
+
+  // Add a transition in the graph to be traversed in the output static
+  // analysis.
+  C.addTransition(State);
 }
 
 void ento::registerObjectCountChecker(CheckerManager &mgr) {
--- clang/test/Analysis/object-count-checks.c
+++ clang/test/Analysis/object-count-checks.c
@@ -1,6 +1,7 @@
 // RUN: %clang_analyze_cc1 -analyzer-checker=alpha.unix.ObjectCount -verify %s
 
 #include "object-header.h"
+#include <stddef.h>
 
 // Cannot return consumed header.
 __OBJECT_CONSUMED header_t* test_attribute_consumed_function_fail(void); // expected-warning {{'object_consumed' attribute only applies to parameters}}
@@ -15,3 +16,17 @@ void test_attribute_returns_acquired_in_param_fail(__OBJECT_RETURN_ACQUIRED head
 // Currently do not validate that the parameters are indeed objects.
 __OBJECT_RETURN_ACQUIRED int test_attribute_arbitrary(
     __OBJECT_CONSUMED int header);
+
+// Do not error saying that we did not consume the header since there is no analysis.
+__OBJECT_NO_ANALYSIS void test_no_leak_error(__OBJECT_CONSUMED header_t* header) {
+};
+
+// Do not error saying that we didn't return acquired reference since there is no analysis.
+__OBJECT_NO_ANALYSIS __OBJECT_RETURN_ACQUIRED header_t* test_no_return_acquired_error() {
+  return NULL;
+};
+
+// Test a variety of parameters to ensure it does not crash.
+void test_arbtirary_parameters(int test1, void* test, header_t* test2, header_t** test3, __OBJECT_CONSUMED header_t* test4) {
+  object_release(test4);
+}
